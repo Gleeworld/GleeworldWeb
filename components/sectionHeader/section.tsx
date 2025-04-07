df632901dcf3a01d393f1939d24f9c7f92896921
David Oladepo <91491738+Dkingofcode@users.noreply.github.com>
2025-04-07 12:38:37 -0700

    Update section.tsx

--- components/sectionHeader/section.tsx
+++ components/sectionHeader/section.tsx
@@ -10,9 +10,6 @@ function Section({ title }: Props) {
     <div className='bg-gray-100 h-500 w-50'>
 
 <div>
-    <h1 className='absolute capitalize top-0 left-0 right-0 bottom-0'>{title}</h1>
-    <Image className='mx-auto w-100 h-100 relative' src="/Mobile Marketing-pana 1.png" alt='hero' width={500} height={500}  />
-
 </div>
 
 <Image className="absolute left-0 top-0 w-40 h-50" src="/Union.png" alt='cross' width={500} height={500} />
