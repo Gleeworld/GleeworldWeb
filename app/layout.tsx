2ffc3642d6d8f3840b307c869dc5b471b468f96b
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-05-28 22:21:48 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         {children}
         <Newsletter />
