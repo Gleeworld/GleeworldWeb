965521e30799aad83bfb4d466bb464d6d63c5f69
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-06-01 21:47:51 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
-import HeroSlider from "@/components/Hero/HeroSlider";
-import Services from '../components/Services/services';
-import Stats from '../components/Stats/stats';
-import NewsSection from '@/components/News/NewsSection';
-import ContactForm from '@/components/Contact/ContactForm';
-import MedfinderPromo from '@/components/Medfinder/MedfinderPromo';
+import Hero1 from "@/components/Hero/Hero1";
+import Services from "@/components/Services/services";
+import Stats from "@/components/Stats/stats";
+import NewsSection from "@/components/News/NewsSection";
+import MedfinderPromo from "@/components/Medfinder/MedfinderPromo";
+import ContactForm from "@/components/Contact/ContactForm";
 
 export default function Home() {
   return (
     <main>
-      <HeroSlider />
+      <Hero1 />
       <Services />
       <Stats />
-      <NewsSection subheading={"latest Developments"} title={"News"} />
+      <NewsSection subheading={"Latest Developments"} title={"News"} />
       <MedfinderPromo />
       <ContactForm
-        title='Contact Us'
-        topText='Get in Touch'
+        title="Contact Us"
+        topText="Get in Touch"
         subHeading="Ready to Elevate your Pharmacy's Operation"
-        details='Contact us today to explore our diverse range of products and discover how our innovative solutions can meet your specific needs'
+        details="Contact us today to explore our diverse range of products and discover how our innovative solutions can meet your specific needs"
       />
     </main>
   );
