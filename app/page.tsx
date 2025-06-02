7b453a2cf78f177f62b2e7272eef3cc68843fa92
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-06-02 21:37:35 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import FlowHero from "@/components/Hero/hero-section";
 
 export default function Page() {
   return (
-    <>
+    <main>
       <FlowHero />
-      {/* Add other sections/components below */}
-    </>
+    </main>
   );
 }
