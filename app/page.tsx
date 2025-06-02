ef562b9fa8bec6d7960f294cf39876e3dc44b3d1
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-06-02 19:16:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
-import Hero1 from "@/components/Hero/Hero1";
-import Services from "@/components/Services/services";
-import Stats from "@/components/Stats/stats";
-import NewsSection from "@/components/News/NewsSection";
-import MedfinderPromo from "@/components/Medfinder/MedfinderPromo";
-import ContactForm from "@/components/Contact/ContactForm";
+import FlowHero from "../components/Hero/hero-section";
+// Other imports...
 
 export default function Home() {
   return (
     <main>
-      <Hero1 />
-      <Services />
-      <Stats />
-      <NewsSection subheading={"Latest Developments"} title={"News"} />
-      <MedfinderPromo />
-      <ContactForm
-        title="Contact Us"
-        topText="Get in Touch"
-        subHeading="Ready to Elevate your Pharmacy's Operation"
-        details="Contact us today to explore our diverse range of products and discover how our innovative solutions can meet your specific needs"
-      />
+      <FlowHero />
+      {/* ... other components */}
     </main>
   );
 }
