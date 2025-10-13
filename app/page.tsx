fd15a01328014e87a9c288267078e93b83f327c4
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-10-13 18:25:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,21 @@ import Stats from "@/components/Stats/stats";
 import NewsSection from "@/components/News/NewsSection";
 import MedfinderPromo from "@/components/Medfinder/MedfinderPromo";
 import ContactForm from "@/components/Contact/ContactForm";
-//import Newsletter from "@/components/NewsLetter/newsForm";
-import Picture from "@/components/Picture/picture";
 
 export default function Page() {
   return (
-    <main>
+    <main className="bg-silver">
       <FlowHero />
-      <Picture  />
       <Services />
       <Stats />
-      <NewsSection title="Title" subheading="header" />
       <MedfinderPromo />
-      <ContactForm title="year" topText="job" subHeading="love life" details="live in the moment"  />
-      
+      <NewsSection title="Healthcare News" subheading="Stay Informed" />
+      <ContactForm 
+        title="Contact Us" 
+        topText="Get in Touch" 
+        subHeading="We're Here to Help" 
+        details="Reach out for any questions about our pharmacy services"  
+      />
     </main>
   );
 }
