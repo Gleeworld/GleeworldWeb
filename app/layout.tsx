ffac3e9e8caf3cdf9eda7ff2ca96b829cae6679b
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-10-15 19:38:07 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Footer from "@/components/Footer/footer";
 
 export const metadata: Metadata = {
   title: "Gleeworld Pharmacy – Telepharmacy and Medication Delivery | Your Dose of Happiness",
-  description: "Gleeworld Pharmacy offers telepharmacy and medication delivery across Nigeria. Use Medfinder to discover nearby pharmacies and get your preferred medicines. Save time with easy pickup or delivery across Nigeria. Experience automated reminders, refills, follow-up care messages, and AI-enabled pharmacist consultations — all in one app. Affordable, fast, and hassle-free access to medicines and primary care.",
+  description:
+    "Gleeworld Pharmacy offers telepharmacy and medication delivery across Nigeria. Use Medfinder to discover nearby pharmacies and get your preferred medicines. Save time with easy pickup or delivery across Nigeria. Experience automated reminders, refills, follow-up care messages, and AI-enabled pharmacist consultations — all in one app. Affordable, fast, and hassle-free access to medicines and primary care.",
   icons: {
     icon: "/gleeworld-favicon.ico",
   },
@@ -24,7 +25,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </head>
-      <body className={`antialiased bg-silver`}> {/* ✅ ADDED bg-silver HERE */}
+      <body className="antialiased bg-white text-dark-gray dark:bg-dark-gray dark:text-white transition-colors duration-300">
         <Navbar />
         {children}
         <Newsletter />
