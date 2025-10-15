580f36637111baecafea88bdac7af22f1687643b
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-10-15 21:26:54 +0100

    Update page.tsx

--- app/(news)/news/page.tsx
+++ app/(news)/news/page.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import NewsSection from '@/components/News/NewsSection';
 import ContactForm from '@/components/Contact/ContactForm';
 
-function page() {
+function Page() {
   return (
     <div>
-      <div className='flex-col text-center items-center gap-5 pt-15'>
-        <p>Trending</p>
-        <h1 className='text-3xl text-black'>Top Stories</h1>
-      </div>
+        <div>
+            <div className='flex flex-col text-center items-center gap-5 pt-15'>
+              <p className="text-lg text-gray-600">Trending</p>
+              <h1 className='text-3xl font-bold text-dark-gray'>Top Stories</h1>
+            </div>
 
-      <NewsSection subheading={"Trending"} title={"Top Stories"} />
+            <NewsSection subheading="Trending" title="Top Stories" />
 
-      <NewsSection subheading={""} title={""} />
+            <NewsSection subheading="Latest Updates" title="Healthcare News" />
 
-      <ContactForm
-        title='Share with us'
-        topText='Do you have credible health news?'
-        subHeading=''
-        details='Please provide as many details as possible to help us understand and verify the information. Our editorial team will review your submission and get back to you promptly.'
-      />
+            <ContactForm 
+              title="Share Health News With Us" 
+              details="Do you have credible health news? Please provide as many details as possible to help us understand and verify the information. Our editorial team will review your submission and get back to you promptly."
+            />
+        </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
