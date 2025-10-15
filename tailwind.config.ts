f84a3e56cf9b5ea0aff18e39a034d1d156beb284
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2025-10-15 19:54:05 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        magneta: '#E10075',
-        orange: '#FF6B35',
-        silver: '#F8F9FA',
-        'dark-gray': '#333333',
+        magneta: "#E10075",
+        orange: "#FF6B35",
+        silver: "#F8F9FA",
+        "dark-gray": "#333333",
       },
     },
   },
   plugins: [],
-}
+};
