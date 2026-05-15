d83a736ea14136682d8f7ca2ac03fe225a3d7050
Gleeworld <149384322+Gleeworld@users.noreply.github.com>
2026-05-15 13:47:26 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Gleeworld Pharmacy – Telepharmacy and Medication Delivery | Your Dose of Happiness",
   description: "Gleeworld Pharmacy offers telepharmacy and medication delivery across Nigeria. Use Medfinder to discover nearby pharmacies and get your preferred medicines. Save time with easy pickup or delivery across Nigeria. Experience automated reminders, refills, follow-up care messages, and AI-enabled pharmacist consultations — all in one app. Affordable, fast, and hassle-free access to medicines and primary care.",
   icons: {
-    icon: "/gleeworld-favicon.ico",
+    icon: "/favicon.ico",
   },
 };
 
@@ -20,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/gleeworld-favicon.ico" type="image/x-icon" />
+        {/* CHANGED THIS LINE BELOW TO MATCH */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </head>
